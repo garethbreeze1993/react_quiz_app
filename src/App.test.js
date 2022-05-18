44e650980b50cfc95b06d0cfc5f65406597b0954
Gareth Breeze <gareth.breeze@hypaship.com>
2022-05-18 17:57:58 +0100

    Start writing some tests for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('check intro elements on page when app is loaded initially', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText('Start Quiz')).toBeInTheDocument();
+  expect(screen.getByText('Quizzical')).toBeInTheDocument();
+  expect(screen.getByText('A fun quiz app built with React')).toBeInTheDocument();
 });
+
+test('check when Start Quiz button clicked that Quiz component loads', async () => {
+  render(<App />);
+  await screen.getByText('Start Quiz')
+  fireEvent.click(screen.getByText('Start Quiz'));
+  expect(screen.getByText('Check Answers')).toBeInTheDocument();
+});
+
+// up to React Testing Library: User Event
+// on https://www.robinwieruch.de/react-testing-library/
