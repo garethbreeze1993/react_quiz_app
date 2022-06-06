82718ed72f4bc7f77aa697b8bc51ef958eaa74c0
Gareth Breeze <gareth.breeze@hypaship.com>
2022-06-06 17:36:46 +0100

    Delete logo and comments. Finish project for now

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import App from './App';
-import {useReducer} from "react";
-import userEvent from "@testing-library/user-event";
 
 const mockQuestionData = [{category: "General Knowledge",
     type: "multiple",
@@ -93,83 +91,3 @@ test('testing user clicks play again and gets sent back to intro screen', async(
   expect(screen.getByText('Quizzical')).toBeInTheDocument();
   expect(screen.getByText('A fun quiz app built with React')).toBeInTheDocument();
 })
-
-// up to React Testing Library: React Testing Library: Asynchronous / Async
-// on https://www.robinwieruch.de/react-testing-library/
-
-
-// < body >
-// < div >
-// < div
-// className = "App"
-//     >
-//     < form >
-//     < div
-// className = "quiz--container"
-//     >
-//     < h5
-// className = "quiz--question"
-//     >
-//     On
-// a
-// dartboard
-// which
-// number
-// is
-// opposite
-// 17 ?
-// </h5>
-// <div
-// className="quiz--answer"
-// >
-// <input
-// id="19"
-// name="On a dartboard which number is opposite 17?"
-// type="radio"
-// value="19"
-// />
-// 19
-// </div>
-// <div
-// className="quiz--answer"
-// >
-// <input
-// id="2"
-// name="On a dartboard which number is opposite 17?"
-// type="radio"
-// value="2"
-// />
-// 2
-// </div>
-// <div
-// className="quiz--answer"
-// >
-// <input
-// id="3"
-// name="On a dartboard which number is opposite 17?"
-// type="radio"
-// value="3"
-// />
-// 3
-// </div>
-// <div
-// className="quiz--answer"
-// >
-// <input
-// id="4"
-// name="On a dartboard which number is opposite 17?"
-// type="radio"
-// value="4"
-// />
-// 4
-// </div>
-// </div>
-// <button
-// className="intro--button"
-// >
-// Check Answers
-// </button>
-// </form>
-// </div>
-// </div>
-// </body>
